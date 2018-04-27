417fe66bd638dffacb86c9413462c95309fc33c2
LC Wadle <lcwadle@gmail.com>
2018-04-27 12:36:13 -0500

    Added python to add tweets to mock_twitter_stream

---
+++ Kafka_helpers/add_tweets.py
@@ -0,0 +1,22 @@
+from kafka import KafkaProducer
+import argparse
+import json
+import os
+
+print "Begin reading tweets.txt to mock_twitter_stream"
+fname = '../raw_data/tweets.txt'
+
+with open(fname, 'r', encoding='utf-8') as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+count = 0
+for tweet in content;
+    count += 1
+    producer.send('mock_twitter_stream', tweet)
+
+producer.flush();
+
+print count
+print "Finished reading to mock_twitter_stream"
--- Kafka_helpers/run_kafka_task.sh
+++ Kafka_helpers/run_kafka_task.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Create mock_twitter_stream topic
-~kafka_task/kafka-0.10.2.0-src/bin/kafka-topics.sh \
-  --create \
-  --zookeeper localhost:2181 \
-  --replication-factor 1 \
-  --partitions 1 \
-  --topic mock_twitter_stream
+#../../bin/kafka-topics.sh \
+#  --create \
+#  --zookeeper localhost:2181 \
+#  --replication-factor 1 \
+#  --partitions 1 \
+#  --topic mock_twitter_stream
 
 # Create mock_twitter_stream topic
-~kafka_task/kafka-0.10.2.0-src/bin/kafka-topics.sh \
-  --create \
-  --zookeeper localhost:2181 \
-  --replication-factor 1 \
-  --partitions 1 \
-  --topic spark_input
+#../../bin/kafka-topics.sh \
+#  --create \
+#  --zookeeper localhost:2181 \
+#  --replication-factor 1 \
+#  --partitions 1 \
+#  --topic spark_input
 
 # Read input file and save in mock_twitter_stream
-#python add_tweets.py
+python add_tweets.py
 
 # Read input file and save in mock_twitter_stream
 #python add_filtered_tweets.py
