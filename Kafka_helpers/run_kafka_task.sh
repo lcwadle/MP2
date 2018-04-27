bfb86839b98e31f2ffccf66d78262fc9eb299e90
LC Wadle <lcwadle@gmail.com>
2018-04-27 12:10:13 -0500

    Update run script to add topics

--- Kafka_helpers/run_kafka_task.sh
+++ Kafka_helpers/run_kafka_task.sh
@@ -1,3 +1,23 @@
-# Kafka Script file
+#!/bin/bash
+
+# Create mock_twitter_stream topic
+$KAFKA_HOME/bin/kafka-topics.sh \
+  --create \
+  --zookeeper localhost:2181 \
+  --replication-factor 1 \
+  --partitions 1 \
+  --topic mock_twitter_stream
+
+# Create mock_twitter_stream topic
+$KAFKA_HOME/bin/kafka-topics.sh \
+  --create \
+  --zookeeper localhost:2181 \
+  --replication-factor 1 \
+  --partitions 1 \
+  --topic spark_input
 
 # Read input file and save in mock_twitter_stream
+#$python add_tweets.py
+
+# Read input file and save in mock_twitter_stream
+#$python add_filtered_tweets.py
