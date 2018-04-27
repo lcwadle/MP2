632a6db8111cded2a684c62d1a257faf00f470fa
LC Wadle <lcwadle@gmail.com>
2018-04-27 13:42:56 -0500

    add_filtered done

--- Kafka_helpers/add_filtered_tweets.py
+++ Kafka_helpers/add_filtered_tweets.py
@@ -1,13 +1,16 @@
-from kafka import KafkaProducer, KafkaConsumer
+from kafka import KafkaProducer, KafkaConsumer, TopicPartition
 import argparse
 import json
 import os
 import io
 
+print "Beginning reading mock_twitter_stream messages into spark_input"
+
 kafka_topic = 'mock_twitter_stream'
 number = 9974
 
 consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
+producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 partitions = consumer.partitions_for_topic(kafka_topic)
 topic_partitions = list()
@@ -28,11 +31,12 @@ for topic_partition, offset in topic_partition_to_offset.items():
 
 count = 0
 for message in consumer:
-    count += 1
 
-    try:
-        value = json.loads(message.value.decode())
-        print value
-        input()
-    except:
-        value = message.value.decode()
+    value = json.loads(message.value.decode())
+
+    if value[2] == 'c':
+        count += 1
+        producer.send('spark_input', value)
+
+print count
+print "Finished reading into spark_input"
